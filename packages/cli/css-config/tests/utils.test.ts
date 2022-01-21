967828070491a9380f09f0db93ff87fe28474c2a
liyubei <liyubei@bytedance.com>
2022-01-22 07:24:33 +0800

    fix: xxx

--- packages/cli/css-config/tests/utils.test.ts
+++ packages/cli/css-config/tests/utils.test.ts
@@ -2,23 +2,23 @@ import type { NormalizedConfig } from '@modern-js/core';
 import { shouldUseSourceMap } from '../src/util';
 
 describe('css-config#util', () => {
-    const e =  process.env.NODE_ENV;
+  const e = process.env.NODE_ENV;
 
-    afterAll(() => {
-        // restore the process.env
-        process.env.NODE_ENV = e;
-    });
+  afterAll(() => {
+    // restore the process.env
+    process.env.NODE_ENV = e;
+  });
 
-    it('shouldUseSourceMap', () => {
-        const config: NormalizedConfig = {
-            output: {
-                disableSourceMap: false
-            }
-        } as any;
-        process.env.NODE_ENV = 'production';
-        expect(shouldUseSourceMap(config)).toBeTruthy();
+  it('shouldUseSourceMap', () => {
+    const config: NormalizedConfig = {
+      output: {
+        disableSourceMap: false,
+      },
+    } as any;
+    process.env.NODE_ENV = 'production';
+    expect(shouldUseSourceMap(config)).toBeTruthy();
 
-        process.env.NODE_ENV = 'development';
-        expect(shouldUseSourceMap(config)).toBeFalsy();
-    })
+    process.env.NODE_ENV = 'development';
+    expect(shouldUseSourceMap(config)).toBeFalsy();
+  });
 });
