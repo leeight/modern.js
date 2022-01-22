ee4cf87908fe6c5bb31d6a243a5cd24572fe3c76
liyubei <liyubei@bytedance.com>
2022-01-22 22:45:19 +0800

    feat: rush build success

--- scripts/build/bin/modern.js
+++ scripts/build/bin/modern.js
@@ -10,11 +10,4 @@ process.env.CORE_INIT_OPTION_FILE = path.resolve(
   '../src/cli_core_init.js',
 );
 
-const distDir = path.join(process.cwd(), 'dist');
-if (!fs.existsSync(distDir)) {
-  require(`${kProjectRoot}/packages/cli/core/src/cli.ts`);
-} else {
-  // eslint-disable-next-line no-console
-  console.log('The build directory `dist` already existed, skip this build');
-  // IGNORE 非常简单的一个缓存策略，只要文件夹存在就不会再次执行
-}
+require(`${kProjectRoot}/packages/cli/core/src/cli.ts`);
