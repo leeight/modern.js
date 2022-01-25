e3313d8bab96292f8a3e0ccf092bbdccdd75e641
leeight <leeight@gmail.com>
2022-01-25 15:08:35 +0800

    feat: xxx

--- packages/cli/core/.eslintrc.js
+++ packages/cli/core/.eslintrc.js
@@ -1,10 +1,5 @@
-// This is a workaround for https://github.com/eslint/eslint/issues/3458
-require('@rushstack/eslint-config/patch/modern-module-resolution');
+const { defineConfig } = require('@scripts/eslint-config');
 
-module.exports = {
-  extends: ['@modern-js'],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: require.resolve('./tsconfig.json'),
-  },
-};
+module.exports = defineConfig({
+   rootDir: __dirname,
+});
---
+++ scripts/eslint-config/index.js
@@ -0,0 +1,34 @@
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('@rushstack/eslint-config/patch/modern-module-resolution');
+
+const fs = require('fs');
+
+function defineConfig(options) {
+  const { rootDir } = options;
+
+  const parserOptions = {
+    tsconfigRootDir: rootDir,
+  };
+
+  const project = `${rootDir}/tsconfig.json`;
+  if (fs.existsSync(project)) {
+    parserOptions.project = project;
+  }
+
+  return {
+    extends: [
+      require.resolve('@modern-js/eslint-config')
+    ],
+    parserOptions,
+  };
+}
+
+exports.defineConfig = defineConfig;
+
+// module.exports = {
+//   extends: ['@modern-js'],
+//   parserOptions: {
+//     tsconfigRootDir: __dirname,
+//     project: require.resolve('./tsconfig.json'),
+//   },
+// };
